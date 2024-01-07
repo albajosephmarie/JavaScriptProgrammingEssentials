675bff46d8473ac1c658ce6b2a1e792bf9e77dd0
Joseph Marie Alba <albajosephmarie@gmail.com>
2024-01-07 14:07:50 -0500

    message

--- jscoursetesting/week3/debug_code.js
+++ jscoursetesting/week3/debug_code.js
@@ -1,25 +1,30 @@
-function performOperation() {
+function performOperation(operator) {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-        let result = multiply(num1, num2);
-
+        let result;
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
         // Display the result
         displayResult(result);
     } else {
         displayResult('Please enter valid numbers');
     }
-}
-
-function multiply(a, b) {
-    // Introduce a debugger statement to pause execution
-    debugger;
-
-    // Multiply the numbers
-    return a * b;
 }
 
 function displayResult(result) {
